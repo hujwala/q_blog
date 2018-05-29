169088b41207a4dac56a840dfb61c1336455b32f
sunil-ramesh <skramesh@qwinix.io>
2018-05-29 10:17:34 +0530

    signup pages implementation

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer as formReducer } from 'redux-form';
+import SignUp from './SignUp/components/SignUp';
+
+const rootReducer = combineReducers({
+    form: formReducer
+});
+
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+<Provider store={store}>
+ <SignUp />
+</Provider>
+,document.getElementById('root'));
