8a0e11cc764bb7176742fdff754655464455e7fe
Prakruthee <prakruthis320@gmail.com>
2018-05-30 15:52:13 +0530

    layout css

--- src/components/Layout/Home.jsx
+++ src/components/Layout/Home.jsx
@@ -8,17 +8,19 @@ class Home extends React.Component {
   render() {
     return (
 			<div className="container">
-		    <div className="row">
+		    <div className="row mt40">
+		    	{/*<div className="col-md-2 col-sm-12 col-lg-2"></div>*/}
 			    <div className="col-md-12 col-sm-12 col-lg-12">
-			    	<h1>Publish your passions, your way</h1>
-						<h3>Create a unique and beautiful blog. It’s easy and free.</h3>
-						<div className="form-group">
-			        <button type="submit"><Link to='/SignUp' className="btn btn-primary" >Create your blog</Link></button>
+			    	<h1 className="header-text">Publish your passions, your way</h1>
+						<h3 className="header-text">Create a unique and beautiful blog.</h3>
+						<div className="form-group text-center">
+			        <Link to='/SignUp' className="btn singup-button">Create your blog</Link>
 			      </div>
 			    </div>
 		    </div>
-		    <div className="row mt80">
-			    <div className="col-md-12 col-sm-12 col-lg-12">
+		    <div className="row">
+		    	<div className="col-md-3 col-sm-12 col-lg-3"></div>
+			    <div className="col-md-4 col-sm-12 col-lg-4">
 			    	<img src={require("../../image/blog.jpg")} alt="boohoo" className="img-responsive" className="image"/>
 			    </div>
 		    </div>
