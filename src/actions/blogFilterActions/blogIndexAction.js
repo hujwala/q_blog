b38861d3f99709c60a9031c5cd59be77f40b8375
sunil-ramesh <skramesh@qwinix.io>
2018-06-19 12:14:22 +0530

    url changes

--- src/actions/blogFilterActions/blogIndexAction.js
+++ src/actions/blogFilterActions/blogIndexAction.js
@@ -7,7 +7,7 @@ export const blogDetails = () => {
     fetch('http://localhost:8080/rest/use',{
       method: "GET",
       headers: {
-        "authorization": "Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdW5pbC5rcjEwMjRAZ21haWwuY29tIiwicm9sZSI6InF3aW5peCJ9.ijogU6btsCfux14d76x5Z9pz4xS2KB2TQ2_6lPveFdr0qyCPew5APmqM6Le6JFczjULNSYFGcm98-gmApgme3Q",
+        "authorization": "Bearer " + read_cookie("auth_token"),
         "Content-Type": "application/json",
         'Access-Control-Allow-Origin':'*'
       }
