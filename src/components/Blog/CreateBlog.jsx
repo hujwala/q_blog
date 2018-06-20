3817d84d4730f40a91353b48d4f49a1653a5167b
Prakruthee <prakruthis320@gmail.com>
2018-06-20 15:19:26 +0530

    changed button type git staus

--- src/components/Blog/CreateBlog.jsx
+++ src/components/Blog/CreateBlog.jsx
@@ -88,49 +88,50 @@ render(){
   let { fields: {title, description, content, genre, image, readingDuration}, handleSubmit, pristine, submitting } = this.props;
   return (
     <div className="container">
-    <div className="row mt80">
-    <div className="col-md-12 col-sm-12 col-lg-12">
-    <form onSubmit={ handleSubmit(props => this.submit(props))} className="blogForm">
-      <div className='title'>Create Blog </div>
-      <div className="form-group" id="title">
-        <Field name="title" component={renderField} label="Title" {...title} className="form-control"/>
-      </div>
-      <div className="form-group" id="description">
-        <Field name="description" component={renderField} label="Description" {...description} className="form-control"/>
-      </div>
-      <div className="form-group" id="genre">
-        <div className="btn-group">
-          <Field name="genre" component="select">
-            <option value="">Select genre</option>
-            <option value="Tech">Tech</option>
-            <option value="Entrepreneurship">Entrepreneurship</option>
-            <option value="Politics">Politics</option>
-            <option value="Design">Design</option>
-            <option value="Popular">Popular</option>
-          </Field>   
+      <div className="row mt80">
+        <div className="col-md-12 col-sm-12 col-lg-12">
+          <form onSubmit={ handleSubmit(props => this.submit(props))} className="blogForm">
+            <div className='title'>Create Blog </div>
+            <div className="form-group" id="title">
+              <Field name="title" component={renderField} label="Title" {...title} className="form-control"/>
+            </div>
+            <div className="form-group" id="description">
+              <Field name="description" component={renderField} label="Description" {...description} className="form-control"/>
+            </div>
+            <div className="form-group" id="genre">
+              <div className="btn-group">
+                <Field name="genre" component="select">
+                  <option value="">Select genre</option>
+                  <option value="Tech">Tech</option>
+                  <option value="Entrepreneurship">Entrepreneurship</option>
+                  <option value="Politics">Politics</option>
+                  <option value="Design">Design</option>
+                  <option value="Popular">Popular</option>
+                </Field>   
+              </div>
+            </div>
+            <div className="form-group" id="image">
+              <ImageUploader
+                      withIcon={true}
+                      buttonType='button'
+                      buttonText='Choose images'
+                      onChange={this.onDrop}
+                      imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                      maxFileSize={5242880}
+                  />
+            </div>
+            <div className="form-group" id="content">
+              <Field name="content" label="Content" {...content} className="form-control" component="textarea" component={renderField}/>
+            </div>
+            <div className="form-group" id="readingDuration">
+              <Field name="readingDuration" component={renderField} type="number" label="Reading duration in munites" placeholder="in munites" {...readingDuration} className="form-control"/>
+            </div>
+            <div className="form-group text-center">
+              <button type="submit" className="btn singin-button">Create Blog</button>
+            </div>
+          </form>
         </div>
       </div>
-      <div className="form-group" id="image">
-        <ImageUploader
-                withIcon={true}
-                buttonText='Choose images'
-                onChange={this.onDrop}
-                imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                maxFileSize={5242880}
-            />
-      </div>
-      <div className="form-group" id="content">
-        <Field name="content" label="Content" {...content} className="form-control" component="textarea" component={renderField}/>
-      </div>
-      <div className="form-group" id="readingDuration">
-        <Field name="readingDuration" component={renderField} type="number" label="Reading duration in munites" placeholder="in munites" {...readingDuration} className="form-control"/>
-      </div>
-      <div className="form-group text-center">
-        <button type="submit" className="btn singin-button">Create Blog</button>
-      </div>
-    </form>
-    </div>
-    </div>
     </div>
   ) 
   }
