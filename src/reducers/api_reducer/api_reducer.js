9c5eebb7678b81fd9bf42d5d41b9a3f18dcc993b
Prakruthee <prakruthis320@gmail.com>
2018-06-22 17:30:09 +0530

    blog index data

--- src/reducers/api_reducer/api_reducer.js
+++ src/reducers/api_reducer/api_reducer.js
@@ -2,15 +2,19 @@ import { BLOG_DETAILS } from '../../actions/type'
 import { BLOG_UPDATE } from '../../actions/type'
 
 const INITIAL_STATE = {
-  details: {}
+  details: []
 }
 
 export default (state = INITIAL_STATE, action) => {
+
   switch(action.type){
+
     case BLOG_DETAILS: 
-      return {...state, [action.payload.details]:action.payload.data};
+      return {...state, details: action.payload};
+
     case  BLOG_UPDATE:
-     return {...state, [action.payload.details]:action.payload.data};
+     return {...state, details: action.payload};
+
     default :
       return state;
   }
